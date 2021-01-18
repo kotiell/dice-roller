0300d97c188fc86f1bb0579b832f272e06587957
kotiell <kotiell@users.noreply.github.com>
2021-01-17 22:37:18 -0500

    It works!

---
+++ resources/js/containers/Homepage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const rollDice = () => {
+  return (
+    (Math.random() * 21).toFixed(0)
+  )
+}
+
+
+const Homepage = () => {
+  return (
+    <div>
+      <p>Your 1d20 roll = {rollDice()}</p>
+
+    </div>
+  )
+}
+
+
+export { Homepage as default }
+
+if (document.getElementById('example')) {
+  ReactDOM.render(<Homepage />, document.getElementById('example'));
+}
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,18 +1,23 @@
-<!doctype html>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<html lang="en">
 <head>
-  <meta charset="utf-8">
-
-  <title>The HTML5 Herald</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
-
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
-  <script src="js/app.js"></script>
+
+  @yield('content')
+    <!-- React root DOM -->
+    <div id="example">
+    </div>
+
+    <!-- React JS -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
 </body>
 </html>
